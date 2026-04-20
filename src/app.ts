d7f3dd02f335e84239782e5d4e1ea1de9780e714
alaamohamed58 <alaamohamed58999@gmail.com>
2026-04-20 10:14:11 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import http from "node:http";
+
+const PORT = 3000;
+
+function bootstrap() {
+  const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if (method === "GET" && url === "/") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Hello World" }));
+      return;
+    }
+
+    
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Not Found" }));
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
+
+bootstrap();
